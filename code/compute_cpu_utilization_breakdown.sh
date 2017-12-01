0456ea2df7969ff6cf6948e5a53cb3ec410aed5f
Kevin Cheng <tcheng8@binghamton.edu>
2017-12-01 17:47:36 -0500

    Compute the breakdowns of cpu utilization based on atopsar
    new file:   compute_cpu_utilization_breakdown.sh

---
+++ code/compute_cpu_utilization_breakdown.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Compute the mean CPU utilization for the following fields based on the
+# descriptions of atopsar manpage.
+# - user.
+# - nice.
+# - sys.
+# - irq.
+# - softirq.
+# - steal.
+# - guest.
+# - wait.
+# - idle.
+# @author Kevin Cheng       (tcheng8@binghamton.edu)
+# @author Spoorti Doddamani (sdoddam1@binghamton.edu)
+# @author Kartik Gopalan    (kartik@binghamton.edu)
+# @since  12/01/2017
+
+
+# Check the command line arguments.
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <TIME STAMP> <CPU UTILIZATION>"
+  exit 1
+fi
+
+# Get the time stamps
+timestamps=($(cat $1))
+
+# Get the cpu utilizations over time.
+cpu_utilizations=$2
+
+# Compute the mean cpu utilization.
+sed -ne "/${timestamps[0]}/, /${timestamps[1]}/p" ${cpu_utilizations} |\
+awk '{user+=$3; nice+=$4; sys+=$5; irq+=$6; softirq+=$7; steal+=$8; guest+=$9; wait+=$10; idle+=$11} END {printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n" ,user/NR, nice/NR, sys/NR, irq/NR, softirq/NR, steal/NR, guest/NR, wait/NR, idle/NR)}'
+
