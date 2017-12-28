0a98c7ce9a767d1d12aca68d9bdbcf97ca9328d6
Kevin Cheng <tcheng8@binghamton.edu>
2017-12-28 16:53:39 -0500

    Implement the first prototype to read and write the VM control fields
    We have implemented two LKMs, which allowed us to read the VM control
    field and disable the HLT exiting.
    
    modified:   Makefile
    modified:   read_vmcs_control_field.c
    new file:   write_vmcs_control_field.c

--- code/write_vmcs_control_field.c
+++ code/write_vmcs_control_field.c
@@ -1,7 +1,11 @@
 /**
- * Read the control fields of current active VMCS.
+ * Write the new bitmap to control fields of current active VMCS.
  *  - CPU based control field (encoding 0x4002).
+ *    + Disable the VM exit due to HLT by setting the bit position 7 to 0, where
+ *    the bit position starts at 0.
  *  - Pin based control field (encoding 0x4000).
+ *    + Disable the VM exit due to the external interrupts by setting the bit
+ *    position 0 to 0, where the bit position starts at 0.
  * Error flags should be checked, after executing a VMX instruction.
  *  - If there is no current VMCS, RFLAGS.CF is set.
  *  - If there is a current VMCS, but an error occurs, RFLAGS.ZF is set and the
@@ -11,7 +15,7 @@
  * @author Kevin Cheng       (tcheng8@binghamton.edu)
  * @author Spoorti Doddamani (sdoddam1@binghamton.edu)
  * @author Kartik Gopalan    (kartik@binghamton.edu)
- * @since  12/22/2017
+ * @since  12/25/2017
  */
 
 #include <linux/init.h>
@@ -21,7 +25,7 @@
 #include <linux/string.h>
 #include <linux/seq_file.h>
 
-#define PROC_ENTRY_NAME "read_vmcs_control_fields"
+#define PROC_ENTRY_NAME "write_vmcs_control_fields"
 #define CF_ERROR 1
 #define ZF_ERROR 2
 #define PIN_BASED_EXECUTION_CONTROL_FIELD 0x4000
@@ -29,9 +33,9 @@
 
 static struct proc_dir_entry *proc_entry;
 static int open(struct inode *, struct file *);
-static int read_vmcs_control_fields(struct seq_file *, void *);
-static int read_execution_control_field(struct seq_file *, u64);
-static void print_execution_control_bitmap(struct seq_file *, u64, int);
+static int write_vmcs_control_fields(struct seq_file *, void *);
+static int write_execution_control_field(struct seq_file *, u64, u64);
+static void print_error(struct seq_file *, int);
  
 static const struct file_operations file_operations = {
   .owner = THIS_MODULE,
@@ -53,34 +57,33 @@ static void __exit cleanup(void) {
 
 static int open(struct inode *iobj, struct file *fobj) {
   printk(KERN_INFO "/proc/%s is opened.\n", PROC_ENTRY_NAME);
-  return single_open(fobj, read_vmcs_control_fields, NULL);
+  return single_open(fobj, write_vmcs_control_fields, NULL);
 }
 
-static void print_execution_control_bitmap(struct seq_file *seq_file_obj,
-                                           u64 execution_control_field_bitmap,
-                                           int error) {
+static void print_error(struct seq_file *seq_file_obj, int error) {
   if (error == 1) {
     seq_printf(seq_file_obj, "ERROR (RFLAGS.CF): no current VMCS.\n");
   }
   else if (error == 2) {
-    seq_printf(seq_file_obj, "ERROR (RFLAGS.ZF): VMREAD error.\n");
+    seq_printf(seq_file_obj, "ERROR (RFLAGS.ZF): VM instruction error.\n");
   }
-  else {
-    seq_printf(seq_file_obj, "VMCS execution control field: %016llX\n",
-               execution_control_field_bitmap);
-  }
+  else {;}
 }
 
-static int read_execution_control_field(struct seq_file *seq_file_obj,
-                                        u64 encoding) {
+static int write_execution_control_field(struct seq_file *seq_file_obj,
+                                         u64 encoding, u64 bit_position) {
   int error = 0;
   int cf_error = CF_ERROR;
   int zf_error = ZF_ERROR;
   u64 execution_control_field_bitmap = 0;
-  seq_printf(seq_file_obj, "Initialize the execution control field: %016llX\n",
-             execution_control_field_bitmap);
-  asm("mov    %2,    %%rax      \n"\
-      "vmread %%rax, %0         \n"\
+  asm("mov     %2,    %%rax     \n"\
+      "vmread  %%rax, %%rbx     \n"\
+      "jc  .carry               \n"\
+      "jz  .zero                \n"\
+      "mov     %5,    %%rcx     \n"\
+      "btr     %%rcx, %%rbx     \n"\
+      "clc                      \n"\
+      "vmwrite %%rbx, %%rax     \n"\
       "jc  .carry               \n"\
       "jz  .zero                \n"\
       "jmp .exit                \n"\
@@ -92,21 +95,30 @@ static int read_execution_control_field(struct seq_file *seq_file_obj,
       "  jmp .exit              \n"\
       ".exit:                   \n"\
       : "=m" (execution_control_field_bitmap), "=m" (error)
-      : "r"  (encoding), "r" (cf_error), "r" (zf_error)
-      : "rax"
+      : "r"  (encoding), "r" (cf_error), "r" (zf_error), "r" (bit_position)
+      : "rax", "rbx", "rcx"
      );
-  print_execution_control_bitmap(seq_file_obj,
-                                 execution_control_field_bitmap,
-                                 error);
+  print_error(seq_file_obj, error);
   return error;
 }
 
-static int read_vmcs_control_fields(struct seq_file *seq_file_obj,
+static int write_vmcs_control_fields(struct seq_file *seq_file_obj,
                                     void *chrptr) {
-  seq_printf(seq_file_obj, "PIN-based execution control field.\n");
-  read_execution_control_field(seq_file_obj, PIN_BASED_EXECUTION_CONTROL_FIELD);
-  seq_printf(seq_file_obj, "CPU-based execution control field.\n");
-  read_execution_control_field(seq_file_obj, CPU_BASED_EXECUTION_CONTROL_FIELD);
+  /* BUG: soft lockup */
+  /* Disable the VM exit due to the external interrupts. */
+  seq_printf(seq_file_obj, "Update PIN-based execution control field.\n");
+  write_execution_control_field(seq_file_obj,
+                                PIN_BASED_EXECUTION_CONTROL_FIELD, 0);
+
+  /* Disable the HLT exiting. */
+  //seq_printf(seq_file_obj, "Update CPU-based execution control field.\n");
+  //write_execution_control_field(seq_file_obj,
+  //                              CPU_BASED_EXECUTION_CONTROL_FIELD, 7);
+
+  /* Disable the VMX preemption timer */
+  //seq_printf(seq_file_obj, "Update PIN-based execution control field.\n");
+  //write_execution_control_field(seq_file_obj,
+  //                              PIN_BASED_EXECUTION_CONTROL_FIELD, 6);
   return 0;
 }
 
@@ -116,4 +128,5 @@ module_exit(cleanup);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kevin Cheng, Spoorti Doddamani, Kartik Gopalan");
 MODULE_VERSION("0.1");
-MODULE_DESCRIPTION("Read the control fields of current active VMCS.");
+MODULE_DESCRIPTION("Write the control fields of current active VMCS.");
+
